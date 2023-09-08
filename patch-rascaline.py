9de4274f6312d2f8cc0f14505ba23d5cdfd52635
Luthaf <luthaf@luthaf.fr>
2023-09-08 17:14:05 +0200

    Rename equistore to metatensor

--- patch-rascaline.py
+++ patch-rascaline.py
@@ -4,13 +4,13 @@ with open("pyproject.toml") as fd:
     data = toml.load(fd)
 
 
-# replace equistore-core @ github with just the package name
+# replace metatensor-core @ github with just the package name
 assert len(data["project"]["dependencies"]) == 1
 assert data["project"]["dependencies"][0].startswith(
-    "equistore-core @ https://github.com/lab-cosmo/equistore"
+    "metatensor-core @ https://github.com/lab-cosmo/metatensor"
 )
 
-data["project"]["dependencies"] = ["equistore-core"]
+data["project"]["dependencies"] = ["metatensor-core"]
 
 
 with open("pyproject.toml", "w") as fd:
